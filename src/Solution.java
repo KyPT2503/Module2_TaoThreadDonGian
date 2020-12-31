49033a4db328ccfdf9d69b8dc9bafd77ca44ceb5
KyPT2503 <62481511+KyPT2503@users.noreply.github.com>
2020-12-31 16:48:29 +0700

    baitap

--- src/Solution.java
+++ src/Solution.java
@@ -1,13 +1,63 @@
-import java.sql.SQLOutput;
-
 public class Solution {
     public static void main(String[] args) {
-        Thread firstThread = new Thread(new NumberGenerator(1));
+        /*Thread firstThread = new Thread(new NumberGenerator(1));
         Thread secondThread = new Thread(new NumberGenerator(2));
-        /*Thread thirdThread = new Thread(new NumberGenerator(3));*/
         secondThread.setPriority(Thread.MAX_PRIORITY);
         firstThread.start();
-        secondThread.start();
+        secondThread.start();*/
+
+        /*Thread firstThread = new Thread(new OddNumber());
+        Thread secondThread = new Thread(new EvenNumber());
+        firstThread.start();
+        try {
+            firstThread.join(2000);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        secondThread.start();*/
+
+        Thread lazyPrimeFactorization = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long startTime = System.currentTimeMillis();
+                for (int i = 0; i < 30; i++) {
+                    if (isPrimeLazy(i)) {
+                        System.out.println("lazyPrimeFactorization found a prime " + i + " in " + (System.currentTimeMillis() - startTime) + " milliseconds.");
+                    }
+                }
+            }
+        });
+        Thread optimizedPrimeFactorization = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long startTime = System.currentTimeMillis();
+                for (int i = 0; i < 30; i++) {
+                    if (isPrimeOptimized(i)) {
+                        System.out.println("optimizedPrimeFactorization found a prime " + i + " in " + (System.currentTimeMillis() - startTime) + " milliseconds.");
+                    }
+                }
+            }
+        });
+
+        optimizedPrimeFactorization.start();
+        lazyPrimeFactorization.start();
+    }
+
+    private static boolean isPrimeLazy(int number) {
+        boolean result = true;
+        if (number < 2) result = false;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) result = false;
+        }
+        return result;
+    }
+
+    private static boolean isPrimeOptimized(int number) {
+        if (number < 2) return false;
+        for (int i = 2; i <= Math.sqrt(number); i += 2) {
+            if (number % i == 0) return false;
+        }
+        return true;
     }
 }
 
@@ -27,6 +77,34 @@ class NumberGenerator implements Runnable {
             } catch (Exception e) {
                 System.out.println(e);
             }
+        }
+    }
+}
+
+class OddNumber implements Runnable {
+    @Override
+    public void run() {
+        try {
+            for (int i = 0; i < 10; i++) {
+                if (i % 2 == 0) System.out.println(i);
+                Thread.sleep(10);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
+class EvenNumber implements Runnable {
+    @Override
+    public void run() {
+        try {
+            for (int i = 0; i < 10; i++) {
+                if (i % 2 == 1) System.out.println(i);
+                Thread.sleep(10);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
         }
     }
 }
